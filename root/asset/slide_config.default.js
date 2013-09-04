90c28893704ff4c5af402e86d837b33887c6843e
fireyy <fireyy@gmail.com>
2013-09-04 14:20:22 +0800

    增加rename.json, 复制配置

--- root/asset/slide_config.default.js
+++ root/asset/slide_config.default.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: '{%= name %}<br>{%= title %}',
+    subtitle: '{%= description %}',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,7 +22,7 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
+    name: '{%= author_name %}',
     company: 'Job Title<br>Google',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
